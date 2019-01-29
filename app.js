dd42db25f269a6a0f33d0d66c9dea51278b81fc3
NBerlin <nickiberlinpolhem@gmail.com>
2019-01-29 13:58:25 +0100

    NickiBot rages into battle

--- app.js
+++ app.js
@@ -2,9 +2,8 @@ var Chess = require('chess.js').Chess;
 var chess = new Chess();
 
 while (!chess.game_over()) {
+  console.log(chess.moves());
   var moves = chess.moves();
   var move = moves[Math.floor(Math.random() * moves.length)];
   chess.move(move);
 }
-console.log(chess.pgn());
-console.log(chess.ascii())
