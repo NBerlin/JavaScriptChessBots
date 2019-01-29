3f4f37175324487079f44a964e7f6dd8a553ed6b
NBerlin <nickiberlinpolhem@gmail.com>
2019-01-29 13:52:33 +0100

    playing with chess.js

---
+++ app.js
@@ -0,0 +1,10 @@
+var Chess = require('chess.js').Chess;
+var chess = new Chess();
+
+while (!chess.game_over()) {
+  var moves = chess.moves();
+  var move = moves[Math.floor(Math.random() * moves.length)];
+  chess.move(move);
+}
+console.log(chess.pgn());
+console.log(chess.ascii())
