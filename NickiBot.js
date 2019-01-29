9b8130528834c19b36d7f1a17fa65008b8b5aad2
NBerlin <nickiberlinpolhem@gmail.com>
2019-01-29 21:58:56 +0100

    Struggling with JS. NickiBot v 0.001.

--- NickiBot.js
+++ NickiBot.js
@@ -1,15 +1,32 @@
 var Chess = require('chess.js').Chess;
-
+var pieceSymbols=[];
 class NickiBot{
     constructor(color){
         this.color=color;
+        this.getPieceSymbols();
     }
     name(){
         return "NickiBot";
     }
-    makeMove(board){
+    getPieceSymbols(){
+         const symbols=["a","b","c","d","e","f","g","h"];
+         const numbers=[1,2,3,4,5,6,7,8]
+        symbols.forEach(obj=>numbers.forEach(nmb=>pieceSymbols.push(obj+nmb)));
+    }
+    getPiecesOnBoard(board){
 
-        return board.moves()[0];
+        //why no work
+        console.log(board.get('a1'));
+        console.log(pieceSymbols.forEach(obj=>board.get(obj)));
+    }
+    makeMove(board){
+        
+        let bestMove=-1;
+        const moves=board.moves();
+        this.getPiecesOnBoard(board);
+        for(let i =0;i<moves.length;i++){
+            
+        }
         
     }
 };
