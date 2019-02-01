9b2e49f2c518efab40d9c99541de4abc5ce7164a
Joel Bångdal <j.bangdal@gmail.com>
2019-02-01 14:31:04 +0100

    some change

--- app.js
+++ app.js
@@ -3,12 +3,16 @@ const chess = new Chess()
 
 const player1 = require('./' + process.argv[2])('w')
 const player2 = require('./' + process.argv[3])('b')
-
+const time = process.argv[4]
 while (!chess.game_over()) {
   if (chess.turn() == 'w') {
     chess.move(player1.makeMove(new Chess(chess.fen())))
   } else {
     chess.move(player2.makeMove(new Chess(chess.fen())))
+  }
+  if (time == 'slow') {
+    console.log(chess.ascii())
+    for (var i = 1000000000 - 1; i >= 0; i--) {}
   }
 }
 
