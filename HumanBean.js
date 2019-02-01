0774c3ea5f99148bd6d6d1c9f3d201a3d4c6ee80
Oskar Damkjaer <oskar.damkjaer@gmail.com>
2019-02-01 14:29:40 +0100

    add human

---
+++ HumanBean.js
@@ -0,0 +1,17 @@
+const readline = require('readline-sync')
+const moveman = ({ color = 'w', name = 'human' }) => ({
+  makeMove: chess => {
+    const moves = chess.moves()
+    console.log('available moves', moves)
+    let move = readline.question('Pick a move\n')
+    while (!moves.includes(move)) {
+      move = readline.question(
+        'that is not valid, please pick something else\n'
+      )
+    }
+    return move
+  },
+  name: () => name
+})
+
+module.exports = color => moveman({ color })
