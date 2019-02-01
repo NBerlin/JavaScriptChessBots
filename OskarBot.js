55e1419c8ecbbdff798c50824d2ee67d40fc32c0
Oskar Damkjaer <oskar.damkjaer@gmail.com>
2019-02-01 13:46:04 +0100

    fix oskar

--- OskarBot.js
+++ OskarBot.js
@@ -1,6 +1,3 @@
-const chess = new require("chess.js").Chess();
-const chs = new require("chess.js").Chess();
-
 const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h"];
 const nums = [1, 2, 3, 4, 5, 6, 7, 8];
 const points = { p: 1, n: 3, b: 3, r: 5, q: 9, k: 45 };
@@ -30,7 +27,7 @@ const movesThatGainPoints = (state, color) => {
 const moveThatsMateInOne = state =>
   state.moves().find(move => {
     state.move(move);
-    const isDone = chs.in_checkmate();
+    const isDone = state.in_checkmate();
     state.undo();
     return isDone;
   });
@@ -39,23 +36,22 @@ const canMateInOne = moveThatsMateInOne;
 const seeFreePiece = () => false;
 
 const moveman = ({ color = "w", name = "oskar" }) => ({
-  makeMove: fen => {
-    chs.load(fen);
+  makeMove: chess => {
     const justMakeTheBestMove = () => {
-      const moves = movesThatGainPoints(chs, "w");
+      const moves = movesThatGainPoints(chess, "w");
       if (moves.length !== 0) {
         return moves[Math.floor(Math.random() * moves.length)];
       } else {
-        const pawns = chs.moves().filter(m => m.length < 3);
+        const pawns = chess.moves().filter(m => m.length < 3);
         if (pawns.length > 0) {
           return pawns[Math.floor(Math.random() * pawns.length)];
         }
-        return chs.moves()[0];
+        return chess.moves()[0];
       }
     };
 
-    if (canMateInOne(chs)) {
-      return moveThatsMateInOne(chs);
+    if (canMateInOne(chess)) {
+      return moveThatsMateInOne(chess);
     }
 
     if (seeFreePiece()) {
@@ -67,40 +63,4 @@ const moveman = ({ color = "w", name = "oskar" }) => ({
   name: () => name
 });
 
-const stupidman = {
-  makeMove: fen => {
-    chs.load(fen);
-    return chs.moves()[Math.floor(Math.random() * chess.moves().length)];
-  },
-  name: "stopid"
-};
-
-const Nickiklass = require("./NickiBot");
-const nicki = new Nickiklass();
-const oskar = moveman({ color: "b" });
-
-while (!chess.game_over()) {
-  const currentFen = chess.fen();
-  let pMove;
-
-  if (chess.turn() == "w") {
-    pMove = nicki.makeMove(currentFen);
-  } else {
-    pMove = oskar.makeMove(currentFen);
-  }
-  chess.move(pMove);
-  console.log(chess.ascii());
-}
-
-console.log(chess.ascii());
-if (chess.in_checkmate()) {
-  if (chess.turn === "w") {
-    console.log("Winner: " + oskar.name());
-  } else {
-    console.log("Winner: " + nicki.name());
-  }
-} else {
-  console.log("It was a draw");
-}
-
-module.exports = moveman;
+module.exports = color => moveman({ color });
--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-var Chess = require("chess.js").Chess;
-var chess = new Chess();
+const Chess = require("chess.js").Chess;
+const chess = new Chess();
 
-const BotOne = require("./" + process.argv[2]);
-const BotTwo = require("./" + process.argv[3]);
-let player1 = new BotOne("w");
-let player2 = new BotTwo("b");
+const player1 = require("./" + process.argv[2])("w");
+const player2 = require("./" + process.argv[3])("b");
+console.log(player1);
 
 while (!chess.game_over()) {
   if (chess.turn() == "w") {
@@ -13,6 +12,7 @@ while (!chess.game_over()) {
     chess.move(player2.makeMove(new Chess(chess.fen())));
   }
 }
+
 console.log(chess.ascii());
 if (chess.in_checkmate()) {
   if (chess.turn == "w") {
