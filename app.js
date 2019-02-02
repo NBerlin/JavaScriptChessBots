ef876375526816db6ceec9ec61d08c927f2dbde4
NBerlin <nickiberlinpolhem@gmail.com>
2019-02-03 00:55:10 +0100

    Fixed so it correctly displays who won

--- app.js
+++ app.js
@@ -4,15 +4,6 @@ const chess = new Chess()
 const player1 = require('./' + process.argv[2])('w')
 const player2 = require('./' + process.argv[3])('b')
 const time = process.argv[4]
-
-const pieceValues = { p: 1, n: 3, b: 3, r: 5, q: 9, k: 40 }
-const colorValue = { w: 1, b: -1 }
-
-// TODO: Make method
-function getAdvantage(chess) {
-  console.log(chess.fen().split(' ')[0])
-}
-
 while (!chess.game_over()) {
   if (chess.turn() == 'w') {
     chess.move(player1.makeMove(new Chess(chess.fen())))
@@ -21,17 +12,17 @@ while (!chess.game_over()) {
   }
   if (time == 'slow') {
     console.log(chess.ascii())
-    console.log('Board value: ', getAdvantage(chess))
     for (var i = 1000000000 - 1; i >= 0; i--) {}
   }
 }
 
 console.log(chess.ascii())
+console.log(chess.turn())
 if (chess.in_checkmate()) {
-  if (chess.turn() != 'w') {
-    console.log('Winner: ' + player1.name())
-  } else {
+  if (chess.turn() == 'w') {
     console.log('Winner: ' + player2.name())
+  } else {
+    console.log('Winner: ' + player1.name())
   }
 } else {
   console.log('It was a draw')
