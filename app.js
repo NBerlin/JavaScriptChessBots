d2d26d3d568b4d45960521872dd5bf3260f3bf53
Viktor Claesson <claesson.viktor@gmail.com>
2019-02-02 19:24:12 +0100

    added todo method :)

--- app.js
+++ app.js
@@ -4,6 +4,15 @@ const chess = new Chess()
 const player1 = require('./' + process.argv[2])('w')
 const player2 = require('./' + process.argv[3])('b')
 const time = process.argv[4]
+
+const pieceValues = { p: 1, n: 3, b: 3, r: 5, q: 9, k: 40 }
+const colorValue = { w: 1, b: -1 }
+
+// TODO: Make method
+function getAdvantage(chess) {
+  console.log(chess.fen().split(' ')[0])
+}
+
 while (!chess.game_over()) {
   if (chess.turn() == 'w') {
     chess.move(player1.makeMove(new Chess(chess.fen())))
@@ -12,7 +21,7 @@ while (!chess.game_over()) {
   }
   if (time == 'slow') {
     console.log(chess.ascii())
-    console.log('Board value: ')
+    console.log('Board value: ', getAdvantage(chess))
     for (var i = 1000000000 - 1; i >= 0; i--) {}
   }
 }
