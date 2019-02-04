514509b1cc3d28943ed26be4fbbc601e900d33ba
Viktor Claesson <claesson.viktor@gmail.com>
2019-02-04 21:13:17 +0100

    so fancy

--- app.js
+++ app.js
@@ -41,6 +41,6 @@ if (chess.in_checkmate()) {
 console.log('---')
 
 console.log(player1.name(), ' (white)')
-console.log('time (s): ', timePlayer1 / 1000.0)
+console.log('time (s): ', timePlayer1 / 1000.0, '\n')
 console.log(player2.name(), ' (black)')
 console.log('time (s): ', timePlayer2 / 1000.0)
