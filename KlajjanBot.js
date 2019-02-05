20ea0511154a2d62c431e00ba299876c6afbddff
Viktor Claesson <claesson.viktor@gmail.com>
2019-02-05 01:33:19 +0100

    pawn position value update

--- KlajjanBot.js
+++ KlajjanBot.js
@@ -5,7 +5,9 @@ const promotion = {
   r: 0,
   q: points['q'] - points['p']
 }
+const colMul = { w: 1, b: -1 }
 const letterToNum = { a: 0, b: 1, c: 2, d: 3, e: 4, f: 5, g: 6, h: 7 }
+const numToLetter = 'abcdefgh'
 const pawnNeighbours = {
   a: ['b'],
   b: ['a', 'c'],
@@ -46,6 +48,16 @@ const qPosition = [
   [21, 23, 23, 23, 23, 23, 23, 21],
   [21, 21, 21, 21, 21, 21, 21, 21]
 ]
+const pPosition = [
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 1, 2, 2, 2, 2, 1, 0],
+  [0, 1, 2, 3, 3, 2, 1, 0],
+  [0, 1, 2, 3, 3, 2, 1, 0],
+  [0, 1, 2, 2, 2, 2, 1, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0]
+]
 
 class KlajjanBot {
   constructor(color) {
@@ -56,44 +68,78 @@ class KlajjanBot {
     return 'KlajjanBot'
   }
 
-  tradeDown(move) {
-    return move.captured
-      ? points[move.captured] - points[move.piece] < 0
-      : false
+  pawnCheckSquare(chess, pos, dx, dy, checkForPawn = false) {
+    const sq = chess.get(this.square({ x: pos.x - dx, y: pos.y - dy }))
+    return sq && sq.color == this.color && (checkForPawn || sq.type == 'p')
+      ? 1
+      : 0
   }
 
-  positionValue(move) {
-    const xFrom = letterToNum[move.from[0]]
-    const yFrom = parseInt(move.from[1]) - 1
-    const xTo = letterToNum[move.to[0]]
-    const yTo = parseInt(move.to[1]) - 1
+  pawnNeighbourValue(chess, pos) {
+    return (
+      this.pawnCheckSquare(chess, pos, -1, colMul[this.color]) +
+      this.pawnCheckSquare(chess, pos, 1, colMul[this.color]) +
+      this.pawnCheckSquare(chess, pos, -1, -colMul[this.color], true) +
+      this.pawnCheckSquare(chess, pos, 1, -colMul[this.color], true) -
+      this.pawnCheckSquare(chess, pos, -1, 0, true) -
+      this.pawnCheckSquare(chess, pos, 1, 0, true)
+    )
+  }
+
+  pawnValue(chess, from, to) {
+    return (
+      this.pawnNeighbourValue(chess, to) - this.pawnNeighbourValue(chess, from)
+    )
+  }
+
+  square(pos) {
+    return numToLetter[pos.x] + (pos.y + 1)
+  }
+
+  position(pos) {
+    return { x: letterToNum[pos[0]], y: parseInt(pos[1]) - 1 }
+  }
+
+  positionValue(chess, move) {
+    const from = this.position(move.from)
+    const to = this.position(move.to)
     switch (move.piece) {
       case 'n':
-        return nPosition[yTo][xTo] - nPosition[yFrom][xFrom]
+        return nPosition[to.y][to.x] - nPosition[from.y][from.x]
       case 'b':
-        return bPosition[yTo][xTo] - bPosition[yFrom][xFrom]
+        return bPosition[to.y][to.x] - bPosition[from.y][from.x]
       case 'q':
-        return qPosition[yTo][xTo] - qPosition[yFrom][xFrom]
+        return qPosition[to.y][to.x] - qPosition[from.y][from.x]
       case 'p':
-        return 0
+        return (
+          pPosition[to.y][to.x] -
+          pPosition[from.y][from.x] +
+          this.pawnValue(chess, from, to)
+        )
       default:
         return 0
     }
   }
 
-  simpleMoveValue(move) {
+  simpleMoveValue(chess, move) {
     return (
       (this.color == move.color ? 1 : -1) *
       ((move.captured ? points[move.captured] : 0) +
         (move.promotion ? promotion[move.promotion] : 0) +
         (move.flags == 'q' ? 4 : 0) +
         (move.flags == 'k' ? 7 : 0) +
-        this.positionValue(move))
+        this.positionValue(chess, move))
     )
   }
 
+  tradeDown(move) {
+    return move.captured
+      ? points[move.captured] - points[move.piece] < 0
+      : false
+  }
+
   moveValue(chess, move, ret = false, level = 0) {
-    let value = this.simpleMoveValue(move)
+    let value = this.simpleMoveValue(chess, move)
 
     if (ret || level > 3) {
       return value
@@ -110,7 +156,7 @@ class KlajjanBot {
         ...chess.moves({ verbose: true }).map(tMove =>
           tMove.captured // && tMove.to == move.to)
             ? this.moveValue(chess, tMove, this.tradeDown(move), level + 1)
-            : this.simpleMoveValue(tMove)
+            : this.simpleMoveValue(chess, tMove)
         )
       )
     }
