b13e0f3b364d3752304c8ff78fac4659aa2292a9
NBerlin <nickiberlinpolhem@gmail.com>
2019-02-06 21:20:04 +0100

    Implemented some recursion and shit

--- NickiBot.js
+++ NickiBot.js
@@ -49,63 +49,129 @@ class NickiBot {
     
   }
   testMoves(chessMoves){
-    let captureMoves=this.simpleCapture(chessMoves);
-    return chessMoves[0];
+    let ggchecker=this.checkCheckMate(chessMoves);
+    if(ggchecker){
+        return ggchecker;
+    }
+    chessMoves=this.setValuesOnMoves(chessMoves);
+    let godMove=chessMoves.reduce((obj1,obj2)=>obj1.rekursionValue>=obj2.rekursionValue?obj1:obj2,-1)
+    if(godMove.rekursionValue>0){
+      return godMove
+    }
+    return this.safeMove(chessMoves)||godMove;
+    //return (this.simpleCapture(chessMoves)||chessMoves[0]);
    // return this.testTwoMoves(chessCopy);
 
   }
-  simpleCapture(chessMoves){
-    chessMoves
-      .filter(move=>move.captured)
-      .map(move=>({
-        ...move,
-        tradeValue: pieceValues[move.captured]-pieceValues[move.piece],
-      }));
-    let evenTrades =chessMoves.filter(move=>move.valueOfCapture===0);
-    console.log(evenTrades);
-    let advantagedTrade=chessMoves
-    .filter(move=>move.valueOfCapture>move.valueOfPiece)
-    .reduce((move,move2)=>move.valueOfCapture>move2.valueOfCapture?move:move2,0);
-    console.log(advantagedTrade);
-    if(advantagedTrade.length>0){
-      console.log(advantagedTrade[0])
-    return advantagedTrade[0].san;
-    }
-    return chessMoves[0].san;
+  bestTradeMove(moves){
+   moves=moves.filter(move=>this.bestTradeMoveHelp(move)!=null);
   }
-  testTwoMoves(chessCopy){
-    let advBeforeMove = this.getAdvantage(chessCopy);
-    let avaMoves = chessCopy.moves();
-    let valueOfFirstMoves=avaMoves.map(move=>this.testFirstMoves(move,chessCopy,advBeforeMove))
-    .map((v,i)=>({value:v,index:i}))
-    .reduce((a,b)=>(a.value>=b.value?a:b));
-    return valueOfFirstMoves;
+  bestTradeMoveHelp(move){
+   chess.move(move.san);
+   move=chess.moves().filter(move=>move.captured);
+   if(move.length>0)
+   move.forEach(move=>{
+      move.rekursionValue=whoWinsCapture(move);
+   });
+
+
+   return move.reduce((move1,move2)=>move1.rekursionValue>move2.rekursionValue?move1:move2);
+  }
+  safeMove(moves){
+moves=moves.filter(obj=>this.safeMoveHelp(obj));
+return moves[Math.floor(Math.random()*moves.length)]
+  }
+  safeMoveHelp(move){
+    chess.move(move.san);
+    move=chess.moves({verbose:true})
+    chess.undo();
+    if(move.filter(move=>move.captured).length>0){
+      return false;
+    }
+    return true;
     
-  }
-  testFirstMoves(move,chessCopy,advBeforeMove){
-    chessCopy.move(move);
-    if(chessCopy.in_checkmate()){
-      return 1000000000000000;
-    }
-    let moves = chessCopy.moves()
-    .map(move2=>this.testSecondMoves(move2,chessCopy,advBeforeMove))
-    .reduce((a, b) =>(a<b ? a : b));
-    chessCopy.undo();
-    return moves;
-  }
-  testSecondMoves(move2,chessCopy,advBeforeMove){
-    chessCopy.move(move2);
-    let returnValue = (this.getAdvantage(chessCopy)-advBeforeMove);
-    chessCopy.undo();
-    return returnValue;
-  }
-  makeZeroValueMove(fen){
 
   }
+  setValuesOnMoves(chessMoves){
+    let newChessMoves=chessMoves
+    .map(move=>({
+      ...move,
+      pieceValue:pieceValues[move.piece],
+      captureValue: (pieceValues[move.captured]||0)+(0+pieceValues[move.promotion]||0),
+      tradeValue: (pieceValues[move.captured]||0)+(0+pieceValues[move.promotion]||0)-pieceValues[move.piece],
+      rekursionValue:this.whoWinsCapture(move,(pieceValues[move.captured]||0)),
+  }));
+  return newChessMoves;
+}
+checkCheckMate(chessMoves){
+  chessMoves = chessMoves.filter(move=>move.san.includes("#"))[0];
+ if(chessMoves){
+  return chessMoves
+ }
+  return false;
+}
+  simpleCapture(chessMoves){
+   return this.getFreePiece(chessMoves)||
+   this.pushAFuckingPawn(chessMoves)||
+   chessMoves[Math.floor(Math.random*chessMoves.length)];
+  }
+  getFreePiece(chessMoves){
+    chessMoves = chessMoves
+    .filter(moves=>moves.captureValue>0)
+    .map(move=>({
+      ...move,
+      freePiece:this.freePiece(move)
+    }))
+    .reduce((move1,move2)=>move1.captureValue>move2.captureValue?move1:move2,0);
+    if(chessMoves.captureValue>0){
+
+      return chessMoves;
+    }
+    return null;
+  }
+  freePiece(move){
+    chess.move(move.san);
+    let chessMoves=chess.moves();
+    chessMoves=chessMoves.filter(secondMove=>secondMove.captured);
+    chess.undo();
+    if(chessMoves){
+      chessMoves.reduce((firstMove,secondMove)=>pieceValues[firstMove.captured]>pieceValues[secondMove.captured]?firstMove:secondMove,0);
+      return move.captureValue-pieceValues[chessMoves.captured];
+    }
+    return move.captureValue;
+  }
+  whoWinsCapture(move,initialTradeValue){ 
+    chess.move(move.san)
+    let chessMoves=chess.moves({verbose:true});
+    chessMoves=chessMoves.filter(obj=>obj.captured);
+    if(chessMoves.length>0){
+      chessMoves=chessMoves.reduce((obj1,obj2)=>pieceValues[obj1.captured]>pieceValues[obj2.captured]?obj1:obj2);
+      if(!chessMoves.color===this.color){
+      initialTradeValue+=pieceValues[chessMoves.captured]+whoWinsCapture(chessMoves[0],initialTradeValue)
+
+      }
+      else{
+        initialTradeValue+=this.whoWinsCapture(chessMoves,initialTradeValue)-pieceValues[chessMoves.captured]
+      }
+    }
+    chess.undo();
+    return initialTradeValue;
+}
+  pushAFuckingPawn(chessMoves){
+    chessMoves=chessMoves.filter(move=>move.piece=='p');
+    return chessMoves[Math.floor(Math.random()*chessMoves.length)]
+  }
+  getTempMoves(move){
+    chess.move(move);
+    let temp = chess.moves();
+    chess.undo();
+    return temp;
+  }
   makeMove(chessCopy) {
-    //return chessCopy.moves()[this.testTwoMoves(chessCopy)['index']];
-    return this.testMoves(chessCopy.moves({verbose:true}));
-     
+    chess=chessCopy;
+    
+    let temp = this.testMoves(chessCopy.moves({verbose:true}));
+    return temp.san;
   }
 }
 const create = color => new NickiBot(color)
