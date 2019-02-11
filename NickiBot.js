4c707a3382122d50068a359b785b138d79274242
NBerlin <nickiberlinpolhem@gmail.com>
2019-02-11 19:54:07 +0100

    Can't play on black side at all but pretty good now on white side

--- NickiBot.js
+++ NickiBot.js
@@ -1,6 +1,67 @@
 var Chess = require('chess.js').Chess
 var chess = new Chess()
-var pieceValues = { p: 1, n: 3, b: 3, r: 5, q: 9, k: 40 }
+var pieceValues = { p: 100, n: 320, b: 350, r: 500, q: 900, k: 20000 }
+var symbolTranslator = { a: 0, b: 1, c: 2, d: 3, e: 4, f: 5, g: 6, h: 7 }
+const pawnMatrix = [
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [50, 50, 50, 50, 50, 50, 50, 50],
+  [10, 10, 20, 30, 30, 20, 10, 10],
+  [5, 5, 10, 25, 25, 10, 5, 5],
+  [0, 0, 0, 20, 20, 0, 0, 0],
+  [5, -5, -10, 0, 0, -10, -5, 5],
+  [5, 10, 10, -20, -20, 10, 10, 5],
+  [0, 0, 0, 0, 0, 0, 0, 0]
+]
+const knightMatrix = [
+  [-50, -40, -30, -30, -30, -30, -40, -50],
+  [-40, -20, 0, 0, 0, 0, -20, -40],
+  [-30, 0, 10, 15, 15, 10, 0, -30],
+  [-30, 5, 15, 20, 20, 15, 5, -30],
+  [-30, 0, 15, 20, 20, 15, 0, -30],
+  [-30, 5, 10, 15, 15, 10, 5, -30],
+  [-40, -20, 0, 5, 5, 0, -20, -40],
+  [-50, -40, -30, -30, -30, -30, -40, -50]
+]
+const bishopMatrix = [
+  [-20, -10, -10, -10, -10, -10, -10, -20],
+  [-10, 0, 0, 0, 0, 0, 0, -10],
+  [-10, 0, 5, 10, 10, 5, 0, -10],
+  [-10, 5, 5, 10, 10, 5, 5, -10],
+  [-10, 0, 10, 10, 10, 10, 0, -10],
+  [-10, 10, 10, 10, 10, 10, 10, -10],
+  [-10, 5, 0, 0, 0, 0, 5, -10],
+  [-20, -10, -10, -10, -10, -10, -10, -20]
+]
+const rookMatrix = [
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [5, 10, 10, 10, 10, 10, 10, 5],
+  [-5, 0, 0, 0, 0, 0, 0, -5],
+  [-5, 0, 0, 0, 0, 0, 0, -5],
+  [-5, 0, 0, 0, 0, 0, 0, -5],
+  [-5, 0, 0, 0, 0, 0, 0, -5],
+  [-5, 0, 0, 0, 0, 0, 0, -5],
+  [0, 0, 0, 5, 5, 0, 0, 0]
+]
+const queenMatrix = [
+  [-20, -10, -10, -5, -5, -10, -10, -20],
+  [-10, 0, 0, 0, 0, 0, 0, -10],
+  [-10, 0, 5, 5, 5, 5, 0, -10],
+  [-5, 0, 5, 5, 5, 5, 0, -5],
+  [0, 0, 5, 5, 5, 5, 0, -5],
+  [-10, 5, 5, 5, 5, 5, 0, -10],
+  [-10, 0, 5, 0, 0, 0, 0, -10],
+  [-20, -10, -10, -5, -5, -10, -10, -20]
+]
+const kingMatrixWhite = [
+  [-30, -40, -40, -50, -50, -40, -40, -30],
+  [-30, -40, -40, -50, -50, -40, -40, -30],
+  [-30, -40, -40, -50, -50, -40, -40, -30],
+  [-30, -40, -40, -50, -50, -40, -40, -30],
+  [-20, -30, -30, -40, -40, -30, -30, -20],
+  [-10, -20, -20, -20, -20, -20, -20, -10],
+  [20, 20, 0, 0, 0, 0, 20, 20],
+  [20, 30, 10, 0, 0, 10, 30, 20]
+]
 class NickiBot {
   constructor(color) {
     this.color = color
@@ -32,8 +93,6 @@ class NickiBot {
     return pieces
   }
 
-  
-  
   checkCheckMate(chessMoves) {
     chessMoves = chessMoves.filter(move => move.san.includes('#'))[0]
     if (chessMoves) {
@@ -82,7 +141,6 @@ class NickiBot {
 
   makeMove(chessCopy) {
     chess = chessCopy
-
     let temp = this.testMoves(chessCopy.moves({ verbose: true }))
     return temp.san
   }
@@ -92,36 +150,153 @@ class NickiBot {
       return ggchecker
     }
 
-
-    chessMoves = this.setValuesOnMoves(chessMoves);
+    chessMoves = this.setValuesOnMoves(chessMoves)
     return chessMoves.reduce((obj1, obj2) =>
-    obj1.rekursionValue >= obj2.rekursionValue ? obj1 : obj2
-  )
+      obj1.tradeValue >= obj2.tradeValue ? obj1 : obj2
+    )
   }
   setValuesOnMoves(chessMoves) {
     let newChessMoves = chessMoves.map(move => ({
       ...move,
-      tradeValue: this.tradeValue(move)
-    }));
+      tradeValue: this.tradeValue(move, this.moveValue(move), 0)
+    }))
     return newChessMoves
   }
-  tradeValue(move){
-    let value = pieceValues[move.captured];
-    chess.move(move.san)
-    let chessMoves=chess.moves({verbose:true})
-    .filter(obj1=>obj1.captured);
-    if(chessMoves){
-      if(chess.turn()==this.color){
-        value-=Math.max(chessMoves.map(obj1=>tradeValue(obj1)))
-      }
-      else{
-        value+=Math.max(chessMoves.map(obj1=>tradeValue(obj1)))
-      }
+  moveValue(move) {
+    let values = move.to.split('')
+    let secondValue = parseInt(values[1])
+    values = parseInt(symbolTranslator[values[0]])
+    let values2 = move.from.split('')
+    let secondValue2 = parseInt(values2[1])
+    values2 = parseInt(symbolTranslator[values2[0]])
+    switch (move.piece) {
+      case 'p':
+        if (this.color == 'w') {
+          return (
+            pawnMatrix[8 - secondValue][values] -
+            pawnMatrix[8 - secondValue2][values2]
+          )
+        } else {
+          return (
+            pawnMatrix[secondValue - 1][8 - values] -
+            pawnMatrix[secondValue2 - 1][values2 + 8]
+          )
+        }
 
-      
+      case 'n':
+        if (this.color == 'w') {
+          return (
+            knightMatrix[8 - secondValue][values] -
+            knightMatrix[8 - secondValue2][values2]
+          )
+        } else {
+          return (
+            knightMatrix[secondValue - 1][8 - values] -
+            knightMatrix[secondValue2 - 1][values2 + 8]
+          )
+        }
+
+      case 'b':
+        if (this.color == 'w') {
+          return (
+            bishopMatrix[8 - secondValue][values] -
+            bishopMatrix[8 - secondValue2][values2]
+          )
+        } else {
+          return (
+            bishopMatrix[secondValue - 1][8 - values] -
+            bishopMatrix[secondValue2 - 1][values2 + 8]
+          )
+        }
+
+      case 'r':
+        if (this.color == 'w') {
+          return (
+            rookMatrix[8 - secondValue][values] -
+            rookMatrix[8 - secondValue2][values2]
+          )
+        } else {
+          return (
+            rookMatrix[secondValue - 1][8 - values] -
+            rookMatrix[secondValue2 - 1][values2 + 8]
+          )
+        }
+
+      case 'q':
+        if (this.color == 'w') {
+          return (
+            queenMatrix[8 - secondValue][values] -
+            queenMatrix[8 - secondValue2][values2]
+          )
+        } else {
+          return (
+            queenMatrix[secondValue - 1][8 - values] -
+            queenMatrix[secondValue2 - 1][values2 + 8]
+          )
+        }
+
+      case 'k':
+        if (this.color == 'w') {
+          return (
+            kingMatrixWhite[8 - secondValue][values - 1] -
+            kingMatrixWhite[8 - secondValue2][values2 - 1]
+          )
+        } else {
+          return (
+            kingMatrixWhite[secondValue - 1][8 - values] -
+            kingMatrixWhite[secondValue2 - 1][values2 + 8]
+          )
+        }
     }
   }
+  tradeValue(move, value, depth) {
+    if (move.color === this.color) {
+      value += pieceValues[move.captured] || 0
+    } else {
+      value = value - pieceValues[move.captured] || 0
+    }
 
+    chess.move(move.san)
+    if (chess.turn() == this.color) {
+      if (chess.in_checkmate()) {
+        chess.undo()
+
+        return -10000000
+      }
+    } else {
+      if (chess.in_checkmate()) {
+        chess.undo()
+
+        return 100000
+      }
+    }
+    let chessMoves = chess.moves({ verbose: true })
+    let ggcheck = this.checkCheckMate(chessMoves)
+    if (ggcheck) {
+      if (ggcheck.color === this.color) {
+        chess.undo()
+        return 100000
+      } else {
+        chess.undo()
+        return -100000000
+      }
+    }
+    chessMoves = chessMoves.filter(obj1 => obj1.captured)
+
+    if (chessMoves && depth < 2) {
+      if (chess.turn() == this.color) {
+        value += chessMoves
+          .map(obj1 => this.tradeValue(obj1, value, depth + 1))
+          .reduce((obj1, obj2) => (obj1 > obj2 ? obj1 : obj2), 0)
+      } else {
+        value += chessMoves
+          .map(obj1 => this.tradeValue(obj1, value, depth + 1))
+          .reduce((obj1, obj2) => (obj1 < obj2 ? obj1 : obj2), 0)
+      }
+    }
+    chess.undo()
+    return value
+  }
 }
 const create = color => new NickiBot(color)
 module.exports = create
