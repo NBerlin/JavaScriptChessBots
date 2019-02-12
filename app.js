8d09da90a2282465e13498cfa4d4cb408a357f9d
Oskar Damkjaer <oskar.damkjaer@gmail.com>
2019-02-12 10:37:44 +0100

    now is pretty

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ while (!chess.game_over()) {
 
   if (arg == 'slow' || arg == 'show') {
     console.log(chess.fen())
-    console.log(chess.ascii())
+    prettyPrint(chess)
   }
 
   if (arg == 'slow') {
@@ -42,7 +42,7 @@ while (!chess.game_over()) {
 }
 
 if (!arg) {
-  console.log(chess.ascii())
+  prettyPrint(chess)
 }
 
 if (chess.in_checkmate()) {
@@ -60,3 +60,26 @@ console.log(player1.name(), ' (white)')
 console.log('time (s): ', timePlayer1 / 1000.0, '\n')
 console.log(player2.name(), ' (black)')
 console.log('time (s): ', timePlayer2 / 1000.0)
+
+const prettyPrint = chess =>
+  console.log(
+    chess
+      .asci()
+      .split('')
+      .map(item => unicodemap[item] || item)
+      .join('')
+  )
+const unicodemap = {
+  K: '\u2654',
+  Q: '\u2655',
+  R: '\u2656',
+  B: '\u2657',
+  N: '\u2658',
+  P: '\u2659',
+  k: '\u265A',
+  q: '\u265B',
+  r: '\u265C',
+  b: '\u265D',
+  n: '\u265E',
+  p: '\u265F'
+}
